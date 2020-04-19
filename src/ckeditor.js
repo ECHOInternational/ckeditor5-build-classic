11920500c5ce237322f128da2518d2d86a8ed891
Nate Flood <nate@famconnect.net>
2020-04-18 22:55:38 -0400

    Dark style with no border

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,7 +5,7 @@
 
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
+import './custom.css';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 // import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
